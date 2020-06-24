c04696fd4eba958c7fd8661e5d35aea5906127b5
Matthew Paterno <mpaterno@live.com>
2020-06-24 15:43:39 -0700

    Initial Commit

---
+++ Pencilkit Demo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Pencilkit Demo
+//
+//  Created by Matthew Paterno on 6/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pencilkit Demo/Pencilkit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pencilkit_DemoApp.swift
+//  Pencilkit Demo
+//
+//  Created by Matthew Paterno on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct Pencilkit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
